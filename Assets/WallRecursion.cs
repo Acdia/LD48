31d34fc041eafb6d70356c798f738fe0ce7e1a47
Matti Uhlig <matti.uhlig@yahoo.de>
2021-04-24 10:40:32 +0200

    Added recursion experiment

---
+++ Assets/WallRecursion.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+
+public class WallRecursion : MonoBehaviour
+{
+
+    public Transform recursor;
+    public bool isInitial = false;
+    [SerializeField] int lor = 15;
+
+    [Space]
+
+    public GameObject wall;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        if(isInitial)
+        {
+
+            Recurse(lor);
+        }
+    }
+
+    public void Recurse(int levelOfRecursion)
+    {
+
+        if(levelOfRecursion <= 0)
+        {
+
+            return;
+        }
+
+        GameObject newWall = Instantiate(wall, recursor.position, recursor.rotation);
+        newWall.GetComponent<WallRecursion>().Recurse(levelOfRecursion - 1);
+    }
+}
