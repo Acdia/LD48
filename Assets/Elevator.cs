7242e9e9208143272548c69cc693ab32cab07e13
Matti Uhlig <matti.uhlig@yahoo.de>
2021-04-24 15:41:05 +0200

    Added Elevator (no function)

---
+++ Assets/Elevator.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Elevator : MonoBehaviour
+{
+
+    Animator anim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        
+        if(other.CompareTag("Player"))
+        {
+
+            //The player is nearby
+            OpenDoor();
+        }
+    }
+
+    void OpenDoor()
+    {
+
+        anim.SetTrigger("OpenDoor");
+    }
+}
